a5ed5e6ea1e856955230686e53e646fa18f56381
XiangYao73 <yao73@uwindsor.ca>
2022-11-20 19:58:16 -0500

    3rd feature implemented

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,8 +1,22 @@
 from django.contrib import admin
 from myapp.models import Product, Category, Client, Order
 
+
+def add_stock(modeladmin, request, queryset):
+    queryset.update(available=True)
+    for obj in queryset:
+        obj.stock += 50
+        obj.save()
+#    add_stock.short_description = "Update available status and add 50 in stock"
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'stock', 'available')
+    actions = [add_stock]
+
+
 # Register your models here.
-admin.site.register(Product)
+admin.site.register(Product, ProductAdmin)
 admin.site.register(Category)
 admin.site.register(Client)
 admin.site.register(Order)
